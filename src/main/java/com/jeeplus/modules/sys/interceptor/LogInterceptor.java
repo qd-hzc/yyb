85ee503575a06ab102bd9aa4afa781c8d4a0dac3
wenbin <1104302665@qq.com>
2019-03-11 14:34:13 +0800

    新接口

--- src/main/java/com/jeeplus/modules/sys/interceptor/LogInterceptor.java
+++ src/main/java/com/jeeplus/modules/sys/interceptor/LogInterceptor.java
@@ -58,6 +58,7 @@ public class LogInterceptor extends BaseService implements HandlerInterceptor {
 		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
+		response.addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, token");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
 		response.setHeader("Access-Control-Allow-Credentials","true"); //是否支持cookie跨域
 
